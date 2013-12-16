3ea309eb87f4c1d2d6a6ffac7a1d3640277c4818
idvorkin <idvorkin@gmail.com>
2013-12-16 06:51:40 -0800

    Updating

--- ListNoteBooks/Program.cs
+++ ListNoteBooks/Program.cs
@@ -15,12 +15,8 @@ namespace ListNoteBooks
     {
         static void Main(string[] args)
         {
-            var app = new Microsoft.Office.Interop.OneNote.Application();
-            string heirXML;
-            app.GetHierarchy("", HierarchyScope.hsNotebooks, out heirXML);
-            var serializer = new XmlSerializer(typeof(Notebooks));
-            var notebooks = (Notebooks)serializer.Deserialize(new System.IO.StringReader(heirXML));
-            notebooks.Notebook.Select(notebook=>notebook.name).ToList().ForEach(Console.WriteLine);
+            var app = new OneNoteApp();
+            app.GetNotebooks().Notebook.Select(notebook=>notebook.name).ToList().ForEach(Console.WriteLine);
         }
     }
 }
---
+++ OneNoteObjectModel/OneNoteApp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using Microsoft.Office.Interop.OneNote;
+using OneNoteObjectModel;
+
+namespace OneNoteObjectModel
+{
+    public class OneNoteApp
+    {
+        public Application onenote = new Microsoft.Office.Interop.OneNote.Application();
+
+        public Application Onenote
+        {
+            get { return onenote; }
+        }
+
+        public Notebooks GetNotebooks()
+        {
+            string hierarchyXmlOut;
+            onenote.GetHierarchy("", HierarchyScope.hsNotebooks, out hierarchyXmlOut);
+            var serializer = new XmlSerializer(typeof (Notebooks));
+            return (Notebooks) serializer.Deserialize(new System.IO.StringReader(hierarchyXmlOut));
+        }
+    }
+}
