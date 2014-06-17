8b7e606394a22d4e8d1e3f3fd661cc23da081cbd
idvorkin <idvorkin@gmail.com>
2014-06-17 10:26:34 -0700

    Updating init to install nunit and nuget packages.

--- init.bat
+++ init.bat
@@ -1,8 +1,18 @@
 @echo OFF
 if EXIST "%VS120COMNTOOLS%" (
-    ECHO FOUND VS12 CALLING
+    ECHO FOUND VS12  - CALLING vcvars32.bat
     call "%VS120COMNTOOLS%\..\..\VC\bin\vcvars32.bat"
 ) ELSE (
-    ECHO NOT FOUND
+    ECHO VS NOT FOUND - BUILD WILL FAIL.
 )
+
+if EXIST "c:\Program Files (x86)\NUnit 2.6.3\bin\nunit-console.exe" (
+    ECHO FOUND NUnit SKIPPING Installation.
+) ELSE (
+    ECHO Installing NUnit via chocolatey
+    choco install NUnit
+)
+
+ECHO NUGET Package Installation.
+call nuget restore
 
