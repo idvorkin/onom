67752d24cc7eb4a395d62c7a012e4ef88dc24876
Igor Dvorkin <idvorkin@gmail.com>
2014-06-27 22:42:46 -0700

    Move SmartTagProcessors to own file.

---
+++ OnenoteCapabilities/SmartTagProcessor.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using OneNoteObjectModel;
+
+namespace OnenoteCapabilities
+{
+    public interface ISmartTagProcessor
+    {
+        // TBD:Ensure matching functions don't overlap (easy source of bugs)
+        bool ShouldProcess(SmartTag st);
+        void Process(SmartTag smartTag, XDocument pageContent, SmartTagAugmenter smartTagAugmenter);
+    }
+
+    public class PeopleSmartTagProcessor : ISmartTagProcessor
+    {
+        private SettingsPeoplePages settings;
+        private Section peopleSection;
+
+        public PeopleSmartTagProcessor(SettingsPeoplePages settings)
+        {
+            this.settings = settings;
+        }
+
+        public bool ShouldProcess(SmartTag st)
+        {
+            return settings.People().Contains(st.TagName());
+        }
+
+        public void Process(SmartTag smartTag, XDocument pageContent, SmartTagAugmenter smartTagAugmenter)
+        {
+            // TBD move to c'tor - but doesnt' yet get an smartTagAugmetor.
+
+            this.peopleSection = smartTagAugmenter.ona.GetNotebooks()
+                .Notebook.First(n => n.name == settings.PeoplePagesNotebook)
+                .PopulatedSections(smartTagAugmenter.ona).First(s => s.name == settings.PeoplePagesSection);
+
+            smartTagAugmenter.AddLinkToSmartTag(pageContent,smartTag,peopleSection,settings.PersonNextTitle(smartTag.TagName()));
+        }
+        
+    }
+
+    public class TwitterSmartTagProcessor: ISmartTagProcessor
+    {
+        public bool ShouldProcess(SmartTag st)
+        {
+            return st.TagName().ToLower() == "tweet";
+        }
+
+        public void Process(SmartTag smartTag, XDocument pageContent, SmartTagAugmenter smartTagAugmenter)
+        {
+
+
+            // do nothing.
+        }
+    }
+}
--- OnenoteCapabilities/SmartTags.cs
+++ OnenoteCapabilities/SmartTags.cs
@@ -162,58 +162,5 @@ namespace OnenoteCapabilities
         private readonly string smartTagRegExPattern = "(#[a-zA-Z0-9]+) ";
         private readonly string fullTextOfSmartTagMatcher = "(#[a-zA-Z0-9\\s]+)";
         private readonly string embedSmartTagIdFormatter = "<a href=\"ObjectId={0}\">.</a>";
-        private readonly string smartTagFindFormatter = "ObjectId={0}\">.</a>";
-        private readonly string infoFormatter = "<![CDATA[{0}]]>";
-
-    }
-
-    public interface ISmartTagProcessor
-    {
-        // TBD:Ensure matching functions don't overlap (easy source of bugs)
-        bool ShouldProcess(SmartTag st);
-        void Process(SmartTag smartTag, XDocument pageContent, SmartTagAugmenter smartTagAugmenter);
-    }
-
-    public class PeopleSmartTagProcessor : ISmartTagProcessor
-    {
-        private SettingsPeoplePages settings;
-        private Section peopleSection;
-
-        public PeopleSmartTagProcessor(SettingsPeoplePages settings)
-        {
-            this.settings = settings;
-        }
-
-        public bool ShouldProcess(SmartTag st)
-        {
-            return settings.People().Contains(st.TagName());
-        }
-
-        public void Process(SmartTag smartTag, XDocument pageContent, SmartTagAugmenter smartTagAugmenter)
-        {
-            // TBD move to c'tor - but doesnt' yet get an smartTagAugmetor.
-
-            this.peopleSection = smartTagAugmenter.ona.GetNotebooks()
-                .Notebook.First(n => n.name == settings.PeoplePagesNotebook)
-                .PopulatedSections(smartTagAugmenter.ona).First(s => s.name == settings.PeoplePagesSection);
-
-            smartTagAugmenter.AddLinkToSmartTag(pageContent,smartTag,peopleSection,settings.PersonNextTitle(smartTag.TagName()));
-        }
-        
-    }
-
-    public class TwitterSmartTagProcessor: ISmartTagProcessor
-    {
-        public bool ShouldProcess(SmartTag st)
-        {
-            return st.TagName().ToLower() == "tweet";
-        }
-
-        public void Process(SmartTag smartTag, XDocument pageContent, SmartTagAugmenter smartTagAugmenter)
-        {
-
-
-            // do nothing.
-        }
     }
 }
