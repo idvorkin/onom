cf1f61006f121cadaec03d3c62f56e8967ba13af
Igor Dvorkin <idvorkin@gmail.com>
2014-07-14 18:50:47 -0700

    Refactoring SmartTag tests to be easier to read.

---
+++ OneNoteObjectModelTests/SmartTagTestsPageConent.cs
@@ -0,0 +1,96 @@
+namespace OneNoteObjectModelTests
+{
+    public class SmartTagTestsPageConent
+    {
+        internal readonly string smartTagModelTemplatePageWithDumbTodoTable = "<one:Page xmlns:one=\"http://schemas.microsoft.com/office/onenote/2013/onenote\" ID=\"{0}\" name=\"{1}\" dateTime=\"2014-06-28T18:47:43.000Z\" lastModifiedTime=\"2014-07-02T13:10:47.000Z\" pageLevel=\"2\" isCurrentlyViewed=\"true\" lang=\"en-US\">"+
+                                                                     "  <one:QuickStyleDef index=\"0\" name=\"PageTitle\" fontColor=\"automatic\" highlightColor=\"automatic\" font=\"Calibri Light\" fontSize=\"20.0\" spaceBefore=\"0.0\" spaceAfter=\"0.0\" />"+
+                                                                     "  <one:QuickStyleDef index=\"1\" name=\"p\" fontColor=\"automatic\" highlightColor=\"automatic\" font=\"Calibri\" fontSize=\"11.0\" spaceBefore=\"0.0\" spaceAfter=\"0.0\" />"+
+                                                                     "  <one:PageSettings RTL=\"false\" color=\"automatic\">"+
+                                                                     "    <one:PageSize>"+
+                                                                     "      <one:Automatic />"+
+                                                                     "    </one:PageSize>"+
+                                                                     "    <one:RuleLines visible=\"false\" />"+
+                                                                     "  </one:PageSettings>"+
+                                                                     "  <one:Title lang=\"en-US\">"+
+                                                                     "    <one:OE author=\"Igor Dvorkin\" authorInitials=\"ID\" authorResolutionID=\"&lt;resolutionId provider=&quot;Windows Live&quot; hash=&quot;XDgdaY/mTnjLm73zUG5SXQ==&quot;&gt;&lt;localId cid=&quot;922579950926bf9e&quot;/&gt;&lt;/resolutionId&gt;\" lastModifiedBy=\"Igor Dvorkin\" lastModifiedByInitials=\"ID\" lastModifiedByResolutionID=\"&lt;resolutionId provider=&quot;Windows Live&quot; hash=&quot;XDgdaY/mTnjLm73zUG5SXQ==&quot;&gt;&lt;localId cid=&quot;922579950926bf9e&quot;/&gt;&lt;/resolutionId&gt;\" creationTime=\"2014-07-02T13:54:19.000Z\" lastModifiedTime=\"2014-07-02T13:54:19.000Z\" alignment=\"left\" quickStyleIndex=\"0\">"+
+                                                                     "      <one:T><![CDATA[{1}]]></one:T>"+
+                                                                     "    </one:OE>"+
+                                                                     "  </one:Title>"+
+                                                                     "  <one:Outline author=\"Igor Dvorkin\" authorInitials=\"ID\" lastModifiedBy=\"Igor Dvorkin\" lastModifiedByInitials=\"ID\" lastModifiedTime=\"2014-07-05T03:15:27.000Z\">"+
+                                                                     "    <one:Position x=\"54.0\" y=\"122.400001525879\" z=\"0\" />"+
+                                                                     "    <one:Size width=\"731.7882080078124\" height=\"35.37544250488281\" />"+
+                                                                     "    <one:OEChildren>"+
+                                                                     "      <one:OE authorResolutionID=\"&lt;resolutionId provider=&quot;Windows Live&quot; hash=&quot;XDgdaY/mTnjLm73zUG5SXQ==&quot;&gt;&lt;localId cid=&quot;922579950926bf9e&quot;/&gt;&lt;/resolutionId&gt;\" lastModifiedByResolutionID=\"&lt;resolutionId provider=&quot;Windows Live&quot; hash=&quot;XDgdaY/mTnjLm73zUG5SXQ==&quot;&gt;&lt;localId cid=&quot;922579950926bf9e&quot;/&gt;&lt;/resolutionId&gt;\" creationTime=\"2014-07-05T03:14:07.000Z\" lastModifiedTime=\"2014-07-05T03:15:27.000Z\" alignment=\"left\">"+
+                                                                     "        <one:Table bordersVisible=\"true\" hasHeaderRow=\"true\" lastModifiedTime=\"2014-07-05T03:15:27.000Z\">"+
+                                                                     "          <one:Columns>"+
+                                                                     "            <one:Column index=\"0\" width=\"445.8600769042969\" isLocked=\"true\" />"+
+                                                                     "            <one:Column index=\"1\" width=\"277.7980651855469\" isLocked=\"true\" />"+
+                                                                     "          </one:Columns>"+
+                                                                     "          <one:Row lastModifiedTime=\"2014-07-05T03:15:19.000Z\">"+
+                                                                     "            <one:Cell lastModifiedTime=\"2014-07-05T03:15:19.000Z\" lastModifiedByInitials=\"ID\">"+
+                                                                     "              <one:OEChildren>"+
+                                                                     "                <one:OE authorResolutionID=\"&lt;resolutionId provider=&quot;Windows Live&quot; hash=&quot;XDgdaY/mTnjLm73zUG5SXQ==&quot;&gt;&lt;localId cid=&quot;922579950926bf9e&quot;/&gt;&lt;/resolutionId&gt;\" lastModifiedByResolutionID=\"&lt;resolutionId provider=&quot;Windows Live&quot; hash=&quot;XDgdaY/mTnjLm73zUG5SXQ==&quot;&gt;&lt;localId cid=&quot;922579950926bf9e&quot;/&gt;&lt;/resolutionId&gt;\" creationTime=\"2014-07-05T03:14:07.000Z\" lastModifiedTime=\"2014-07-05T03:15:19.000Z\" alignment=\"left\" quickStyleIndex=\"1\" style=\"font-family:Calibri;font-size:11.0pt\">"+
+                                                                     "                  <one:T><![CDATA[<span"+
+                                                                     "style='font-weight:bold'>Task</span>]]></one:T>"+
+                                                                     "                </one:OE>"+
+                                                                     "              </one:OEChildren>"+
+                                                                     "            </one:Cell>"+
+                                                                     "            <one:Cell lastModifiedTime=\"2014-07-05T03:15:19.000Z\" lastModifiedByInitials=\"ID\">"+
+                                                                     "              <one:OEChildren>"+
+                                                                     "                <one:OE authorResolutionID=\"&lt;resolutionId provider=&quot;Windows Live&quot; hash=&quot;XDgdaY/mTnjLm73zUG5SXQ==&quot;&gt;&lt;localId cid=&quot;922579950926bf9e&quot;/&gt;&lt;/resolutionId&gt;\" lastModifiedByResolutionID=\"&lt;resolutionId provider=&quot;Windows Live&quot; hash=&quot;XDgdaY/mTnjLm73zUG5SXQ==&quot;&gt;&lt;localId cid=&quot;922579950926bf9e&quot;/&gt;&lt;/resolutionId&gt;\" creationTime=\"2014-07-05T03:14:07.000Z\" lastModifiedTime=\"2014-07-05T03:15:19.000Z\" alignment=\"left\" quickStyleIndex=\"1\" style=\"font-family:Calibri;font-size:11.0pt\">"+
+                                                                     "                  <one:T><![CDATA[<span"+
+                                                                     "style='font-weight:bold'>Date</span>]]></one:T>"+
+                                                                     "                </one:OE>"+
+                                                                     "              </one:OEChildren>"+
+                                                                     "            </one:Cell>"+
+                                                                     "          </one:Row>"+
+                                                                     "        </one:Table>"+
+                                                                     "      </one:OE>"+
+                                                                     "      <one:OE authorResolutionID=\"&lt;resolutionId provider=&quot;Windows Live&quot; hash=&quot;XDgdaY/mTnjLm73zUG5SXQ==&quot;&gt;&lt;localId cid=&quot;922579950926bf9e&quot;/&gt;&lt;/resolutionId&gt;\" lastModifiedByResolutionID=\"&lt;resolutionId provider=&quot;Windows Live&quot; hash=&quot;XDgdaY/mTnjLm73zUG5SXQ==&quot;&gt;&lt;localId cid=&quot;922579950926bf9e&quot;/&gt;&lt;/resolutionId&gt;\" creationTime=\"2014-07-05T03:15:27.000Z\" lastModifiedTime=\"2014-07-05T03:15:27.000Z\" alignment=\"left\" quickStyleIndex=\"1\">"+
+                                                                     "        <one:T><![CDATA[]]></one:T>"+
+                                                                     "      </one:OE>"+
+                                                                     "    </one:OEChildren>"+
+                                                                     "  </one:Outline>"+
+                                                                     "</one:Page>";
+
+        internal readonly string pageWithOneProcessedAndOneUnProcessedSmartTag = "<one:Page xmlns:one=\"http://schemas.microsoft.com/office/onenote/2013/onenote\" ID=\"{0}\" name=\"Scratch\" dateTime=\"2014-06-28T18:47:43.000Z\" lastModifiedTime=\"2014-06-28T19:00:46.000Z\" pageLevel=\"2\" isCurrentlyViewed=\"true\" lang=\"en-US\">"+
+                                                                        "  <one:QuickStyleDef index=\"0\" name=\"PageTitle\" fontColor=\"automatic\" highlightColor=\"automatic\" font=\"Calibri Light\" fontSize=\"20.0\" spaceBefore=\"0.0\" spaceAfter=\"0.0\" />"+
+                                                                        "  <one:QuickStyleDef index=\"1\" name=\"p\" fontColor=\"automatic\" highlightColor=\"automatic\" font=\"Calibri\" fontSize=\"11.0\" spaceBefore=\"0.0\" spaceAfter=\"0.0\" />"+
+                                                                        "  <one:PageSettings RTL=\"false\" color=\"automatic\">"+
+                                                                        "    <one:PageSize>"+
+                                                                        "      <one:Automatic />"+
+                                                                        "    </one:PageSize>"+
+                                                                        "    <one:RuleLines visible=\"false\" />"+
+                                                                        "  </one:PageSettings>"+
+                                                                        "  <one:Title lang=\"en-US\">"+
+                                                                        "    <one:OE author=\"Igor Dvorkin\" authorInitials=\"ID\" authorResolutionID=\"&lt;resolutionId provider=&quot;Windows Live&quot; hash=&quot;XDgdaY/mTnjLm73zUG5SXQ==&quot;&gt;&lt;localId cid=&quot;922579950926bf9e&quot;/&gt;&lt;/resolutionId&gt;\" lastModifiedBy=\"Igor Dvorkin\" lastModifiedByInitials=\"ID\" lastModifiedByResolutionID=\"&lt;resolutionId provider=&quot;Windows Live&quot; hash=&quot;XDgdaY/mTnjLm73zUG5SXQ==&quot;&gt;&lt;localId cid=&quot;922579950926bf9e&quot;/&gt;&lt;/resolutionId&gt;\" creationTime=\"2014-06-28T19:00:35.000Z\" lastModifiedTime=\"2014-06-28T19:00:35.000Z\" alignment=\"left\" quickStyleIndex=\"0\">"+
+                                                                        "      <one:T><![CDATA[{1}]]></one:T>"+
+                                                                        "    </one:OE>"+
+                                                                        "  </one:Title>"+
+                                                                        "  <one:Outline author=\"Igor Dvorkin\" authorInitials=\"ID\" lastModifiedBy=\"Igor Dvorkin\" lastModifiedByInitials=\"ID\" lastModifiedTime=\"2014-06-28T19:00:44.000Z\">"+
+                                                                        "    <one:Position x=\"36.0\" y=\"86.4000015258789\" z=\"0\" />"+
+                                                                        "    <one:Size width=\"167.2590484619141\" height=\"67.13858032226562\" />"+
+                                                                        "    <one:OEChildren>"+
+                                                                        "      <one:OE authorResolutionID=\"&lt;resolutionId provider=&quot;Windows Live&quot; hash=&quot;XDgdaY/mTnjLm73zUG5SXQ==&quot;&gt;&lt;localId cid=&quot;922579950926bf9e&quot;/&gt;&lt;/resolutionId&gt;\" lastModifiedByResolutionID=\"&lt;resolutionId provider=&quot;Windows Live&quot; hash=&quot;XDgdaY/mTnjLm73zUG5SXQ==&quot;&gt;&lt;localId cid=&quot;922579950926bf9e&quot;/&gt;&lt;/resolutionId&gt;\" creationTime=\"2014-06-28T19:00:35.000Z\" lastModifiedTime=\"2014-06-28T19:00:35.000Z\" alignment=\"left\" quickStyleIndex=\"1\">"+
+                                                                        "        <one:T><![CDATA[]]></one:T>"+
+                                                                        "      </one:OE>"+
+                                                                        "      <one:OE authorResolutionID=\"&lt;resolutionId provider=&quot;Windows Live&quot; hash=&quot;XDgdaY/mTnjLm73zUG5SXQ==&quot;&gt;&lt;localId cid=&quot;922579950926bf9e&quot;/&gt;&lt;/resolutionId&gt;\" lastModifiedByResolutionID=\"&lt;resolutionId provider=&quot;Windows Live&quot; hash=&quot;XDgdaY/mTnjLm73zUG5SXQ==&quot;&gt;&lt;localId cid=&quot;922579950926bf9e&quot;/&gt;&lt;/resolutionId&gt;\" creationTime=\"2014-06-28T19:00:35.000Z\" lastModifiedTime=\"2014-06-28T19:00:35.000Z\" alignment=\"left\" quickStyleIndex=\"1\">"+
+                                                                        "        <one:T><![CDATA[This is an empty page with a ]]></one:T>"+
+                                                                        "      </one:OE>"+
+                                                                        "      <one:OE authorResolutionID=\"&lt;resolutionId provider=&quot;Windows Live&quot; hash=&quot;XDgdaY/mTnjLm73zUG5SXQ==&quot;&gt;&lt;localId cid=&quot;922579950926bf9e&quot;/&gt;&lt;/resolutionId&gt;\" lastModifiedByResolutionID=\"&lt;resolutionId provider=&quot;Windows Live&quot; hash=&quot;XDgdaY/mTnjLm73zUG5SXQ==&quot;&gt;&lt;localId cid=&quot;922579950926bf9e&quot;/&gt;&lt;/resolutionId&gt;\" creationTime=\"2014-06-28T19:00:44.000Z\" lastModifiedTime=\"2014-06-28T19:00:44.000Z\" alignment=\"left\" quickStyleIndex=\"1\">"+
+                                                                        "        <one:T><![CDATA[#unprocessedTag Tag2]]></one:T>"+
+                                                                        "      </one:OE>"+
+                                                                        "      <one:OE authorResolutionID=\"&lt;resolutionId provider=&quot;Windows Live&quot; hash=&quot;XDgdaY/mTnjLm73zUG5SXQ==&quot;&gt;&lt;localId cid=&quot;922579950926bf9e&quot;/&gt;&lt;/resolutionId&gt;\" lastModifiedByResolutionID=\"&lt;resolutionId provider=&quot;Windows Live&quot; hash=&quot;XDgdaY/mTnjLm73zUG5SXQ==&quot;&gt;&lt;localId cid=&quot;922579950926bf9e&quot;/&gt;&lt;/resolutionId&gt;\" creationTime=\"2014-06-28T19:00:14.000Z\" lastModifiedTime=\"2014-06-28T19:00:35.000Z\" alignment=\"left\" quickStyleIndex=\"1\">"+
+                                                                        "        <one:T><![CDATA[<a"+
+                                                                        "href=\"onenote:#processedTag|6/28/2014 12:00:31 PM&amp;base-path=https://d.docs.live.net/922579950926bf9e/Documents/BlogContentAndResearch/SmartTagStorage.one\">#</a><a"+
+                                                                        "href=\"onenote:#processedTag&amp;base-path=https://d.docs.live.net/922579950926bf9e/Documents/BlogContentAndResearch/Current.one\">processedTag</a> tag1<a"+
+                                                                        "href=\"http://ObjectId=e9692a18-92ef-47cc-b6e8-351626931e41\">.</a>]]></one:T>"+
+                                                                        "      </one:OE>"+
+                                                                        "      <one:OE authorResolutionID=\"&lt;resolutionId provider=&quot;Windows Live&quot; hash=&quot;XDgdaY/mTnjLm73zUG5SXQ==&quot;&gt;&lt;localId cid=&quot;922579950926bf9e&quot;/&gt;&lt;/resolutionId&gt;\" lastModifiedByResolutionID=\"&lt;resolutionId provider=&quot;Windows Live&quot; hash=&quot;XDgdaY/mTnjLm73zUG5SXQ==&quot;&gt;&lt;localId cid=&quot;922579950926bf9e&quot;/&gt;&lt;/resolutionId&gt;\" creationTime=\"2014-06-28T19:00:35.000Z\" lastModifiedTime=\"2014-06-28T19:00:35.000Z\" alignment=\"left\" quickStyleIndex=\"1\">"+
+                                                                        "        <one:T><![CDATA[]]></one:T>"+
+                                                                        "      </one:OE>"+
+                                                                        "    </one:OEChildren>"+
+                                                                        "  </one:Outline>"+
+                                                                        "</one:Page>";
+    }
+}
