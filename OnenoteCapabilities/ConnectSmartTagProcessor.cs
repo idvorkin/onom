03cb4dfd6502ed5754a7112b9e3376d0b648ddbf
idvorkin <idvorkin@gmail.com>
2014-08-18 21:51:10 -0700

    Augmenting #connect with MAPI name resolution.

--- OnenoteCapabilities/ConnectSmartTagProcessor.cs
+++ OnenoteCapabilities/ConnectSmartTagProcessor.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Xml.Linq;
+using Microsoft.Office.Interop.Outlook;
 
 namespace OnenoteCapabilities
 {
@@ -13,16 +14,62 @@ namespace OnenoteCapabilities
             return st.TagName() == "connect";
         }
 
+        private string ResolveNameViaMapi(string userName)
+        {
+            // TBD: Learn how to get email suffix via mapi.
+            var emailSuffix = "@microsoft.com";
+
+            var outlook = new Application();
+            var mapi = outlook.GetNamespace("MAPI");
+            var recipient = mapi.CreateRecipient(userName);
+
+
+            bool isSimpleResolution = recipient.Resolve();
+            if (isSimpleResolution)
+            {
+                return recipient.AddressEntry.GetExchangeUser().Alias + emailSuffix;
+            }
+
+            // show resolution dialog.
+            var resolverDialog = outlook.Session.GetSelectNamesDialog();
+            resolverDialog.Recipients.Add(userName);
+            if (resolverDialog.Recipients.ResolveAll())
+            {
+                return resolverDialog.Recipients[1].AddressEntry.GetExchangeUser().Alias + emailSuffix;
+            }
+            
+            // all resolutions failed - return null.
+            return null;
+        }
+
         public void Process(SmartTag smartTag, XDocument pageContent, SmartTagAugmenter smartTagAugmenter, OneNotePageCursor cursor)
         {
-            // TBD: FIgure out what to do if not an email address.
-            var mailLink = String.Format("mailto:{0}", smartTag.TextAfterTag());
+            // a weak test.
+            var isAnEmailAddress = (smartTag.TextAfterTag().Contains("@"));
+            string emailAddress = "";
+            if (isAnEmailAddress)
+            {
+                emailAddress = smartTag.TextAfterTag();
+            }
+            else
+            {
+                // let mapi mapi do resolution.
+                emailAddress = ResolveNameViaMapi(smartTag.TextAfterTag());
+            }
+
+            if (String.IsNullOrEmpty(emailAddress))
+            {
+                smartTag.AddContentAfter("<b>Error:</b> unable to resolve name");
+                return;
+            }
+
+            var mailLink = String.Format("mailto:{0}", emailAddress);
             smartTag.SetLink(new Uri(mailLink));
         }
 
         public string HelpLine()
         {
-            return "<b>#connect</b> make a connection to the person";
+            return "<b>#connect</b> make a connection to the person or email address";
         }
     }
 }
