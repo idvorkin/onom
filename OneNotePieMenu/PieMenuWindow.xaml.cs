eef73d140a6997b66e9c976dbc8fe8f25b462be9
idvorkin <idvorkin@gmail.com>
2014-10-22 09:39:12 -0700

    Adding Google Analyitcs For PieMenu
    CSharpAnalytics Info:  https://github.com/AttackPattern/CSharpAnalytics private build @ https://github.com/AttackPattern/CSharpAnalytics/commit/c9e38dbc0ace2bfc87ca2be4f6adf276e8831a73

--- OneNotePieMenu/PieMenuWindow.xaml.cs
+++ OneNotePieMenu/PieMenuWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -13,6 +15,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using CSharpAnalytics;
+using CSharpAnalytics.Sessions;
 using OneNoteMenu;
 using OneNoteObjectModel;
 using OnenoteCapabilities;
@@ -25,15 +29,56 @@ namespace OneNotePieMenu
     /// </summary>
     public partial class PieMenuWindow : Window
     {
+        // copied from: http://stackoverflow.com/questions/1600962/displaying-the-build-date
+        private DateTime RetrieveLinkerTimestamp()
+        {
+            string filePath = System.Reflection.Assembly.GetCallingAssembly().Location;
+            const int c_PeHeaderOffset = 60;
+            const int c_LinkerTimestampOffset = 8;
+            byte[] b = new byte[2048];
+            System.IO.Stream s = null;
+
+            try
+            {
+                s = new System.IO.FileStream(filePath, System.IO.FileMode.Open, System.IO.FileAccess.Read);
+                s.Read(b, 0, 2048);
+            }
+            finally
+            {
+                if (s != null)
+                {
+                    s.Close();
+                }
+            }
+
+            int i = System.BitConverter.ToInt32(b, c_PeHeaderOffset);
+            int secondsSince1970 = System.BitConverter.ToInt32(b, i + c_LinkerTimestampOffset);
+            DateTime dt = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            dt = dt.AddSeconds(secondsSince1970);
+            dt = dt.ToLocalTime();
+            return dt;
+        }
+
 
         public static RoutedCommand AugmentCommand = new RoutedCommand();
         public ObservableCollection<string> _observablePeople;
         public AllOneNoteCapabilities capabilities = null;
 
+        public void InitAnalytics()
+        {
+            AutoMeasurement.Instance = new WpfAutoMeasurement();
+            AutoMeasurement.DebugWriter = d => Debug.WriteLine(d);
+
+            // UA... is the APP-ID for OneNoteLabs/OneNotePieMenu - to get access mail idvorkin
+            var config = new MeasurementConfiguration("UA-53825421-1","OneNotePieMenu", RetrieveLinkerTimestamp().ToString());
+            AutoMeasurement.Start(config);
+        }
+
         public PieMenuWindow()
         {
-
+            var initTimer = Stopwatch.StartNew();
             InitializeComponent();
+            InitAnalytics();
             capabilities = new AllOneNoteCapabilities();
             _observablePeople = new ObservableCollection<string>(capabilities.ListOfPeople);
             this.PeopleList.ItemsSource = _observablePeople;
@@ -41,6 +86,7 @@ namespace OneNotePieMenu
             RefreshTopicLruMenu();
             CrashDumpWriter.InstallReportAndCreateCrashDumpUnhandledExceptionHandler();
             AugmentCommand.InputGestures.Add( new KeyGesture( Key.A , ModifierKeys.Control ));
+            AutoMeasurement.Client.TrackTimedEvent("Init","Duration",initTimer.Elapsed);
         }
 
 
@@ -52,6 +98,7 @@ namespace OneNotePieMenu
         /// <param name="e"></param>
         private void MainWindow_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
+            AutoMeasurement.Client.TrackEvent("Windows", MethodBase.GetCurrentMethod().Name);
             // The goal is to be able to come up with a way to move the window given it has windowstyle=none (which is required for transparency).
             // DragMove is the way to instantiate a move, but the only time I can get DragMove() to work is from primary mouse button down.
             base.OnMouseLeftButtonDown(e);
@@ -70,6 +117,7 @@ namespace OneNotePieMenu
 
         private void PieMenuWindow_OnMouseRightButtonDown(object sender, MouseButtonEventArgs e)
         {
+            AutoMeasurement.Client.TrackEvent("Windows", MethodBase.GetCurrentMethod().Name);
             var point = e.MouseDevice.GetPosition(this.Menu1);
             var isOnMenu = this.Menu1.IsMenuRelativePointOnMenu(point);
             if (isOnMenu)
@@ -88,25 +136,30 @@ namespace OneNotePieMenu
 
         private void TimeMachine_OnClick(object sender, RoutedEventArgs e)
         {
+            AutoMeasurement.Client.TrackEvent("DailyPages", MethodBase.GetCurrentMethod().Name);
             ToggleTimeMachineVisibility();
         }
         private void Today_Click(object sender, RoutedEventArgs e)
         {
+            AutoMeasurement.Client.TrackEvent("DailyPages", MethodBase.GetCurrentMethod().Name);
             capabilities.DailyPages.GotoTodayPage();
         }
 
         private void Yesterday_OnClick(object sender, RoutedEventArgs e)
         {
+            AutoMeasurement.Client.TrackEvent("DailyPages", MethodBase.GetCurrentMethod().Name);
             capabilities.DailyPages.GotoYesterday();
         }
 
         private void ThisWeek_OnClick(object sender, RoutedEventArgs e)
         {
+            AutoMeasurement.Client.TrackEvent("DailyPages", MethodBase.GetCurrentMethod().Name);
             capabilities.DailyPages.GotoThisWeekPage();
         }
 
         private void ToggleTimeMachineVisibility()
         {
+            AutoMeasurement.Client.TrackEvent("DailyPages", MethodBase.GetCurrentMethod().Name);
             if (TimeMachineDatePicker.IsVisible)
             {
                 TimeMachineDatePicker.Visibility = Visibility.Hidden;
@@ -124,21 +177,26 @@ namespace OneNotePieMenu
 
         private void Augment_OnClick(object sender, RoutedEventArgs e)
         {
+            var timer = Stopwatch.StartNew();
             capabilities.Augmenter.AugmentCurrentPage();
+            AutoMeasurement.Client.TrackTimedEvent("Augment","Duration",timer.Elapsed);
         }
 
         private void PersonNext_OnClick(object sender, RoutedEventArgs e)
         {
+            AutoMeasurement.Client.TrackEvent("PeoplePages", MethodBase.GetCurrentMethod().Name);
             capabilities.PeoplePages.GotoPersonNextPage(selectedPerson());
         }
 
         private void PersonToday_OnClick(object sender, RoutedEventArgs e)
         {
+            AutoMeasurement.Client.TrackEvent("PeoplePages", MethodBase.GetCurrentMethod().Name);
             capabilities.PeoplePages.GotoPersonCurrentMeetingPage(selectedPerson());
         }
 
         private void PersonPrev_OnClick(object sender, RoutedEventArgs e)
         {
+            AutoMeasurement.Client.TrackEvent("PeoplePages", MethodBase.GetCurrentMethod().Name);
             capabilities.PeoplePages.GotoPersonPreviousMeetingPage(selectedPerson());
         }
 
@@ -171,6 +229,7 @@ namespace OneNotePieMenu
 
         private void GotoTopic_OnClick(object sender, RoutedEventArgs e)
         {
+            AutoMeasurement.Client.TrackEvent("TopicPages", MethodBase.GetCurrentMethod().Name);
             var menuItem = sender as PieInTheSky.PieMenuItem;
             var pageTitle = menuItem.Header as string;
 
@@ -185,6 +244,7 @@ namespace OneNotePieMenu
 
         private void Topic_OnClick(object sender, RoutedEventArgs e)
         {
+            AutoMeasurement.Client.TrackEvent("TopicPages", MethodBase.GetCurrentMethod().Name);
             RefreshTopicLruMenu();
         }
 
@@ -211,6 +271,7 @@ namespace OneNotePieMenu
 
         private void MinimizeClicked(object sender, RoutedEventArgs e)
         {
+            AutoMeasurement.Client.TrackEvent("Windows", MethodBase.GetCurrentMethod().Name);
             this.WindowState = WindowState.Minimized;
         }
 
@@ -221,11 +282,13 @@ namespace OneNotePieMenu
 
         private void ExitClicked(object sender, RoutedEventArgs e)
         {
+            AutoMeasurement.Client.TrackEvent("Windows", MethodBase.GetCurrentMethod().Name);
             Application.Current.Shutdown();
         }
 
         private void OnTopClicked(object sender, RoutedEventArgs e)
         {
+            AutoMeasurement.Client.TrackEvent("Windows", MethodBase.GetCurrentMethod().Name);
             this.Topmost = true;
             // Update the Opacity so we can see edges when on top.
             var backGroundBrush = (this.Background as SolidColorBrush);
