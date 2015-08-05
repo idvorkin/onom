7d495d99bcb0f1f84189998dae6a68e63fbb54c5
unknown <igord@igord2.redmond.corp.microsoft.com>
2015-08-05 11:06:39 -0700

    Update to VS14, latest Nuget/Nunit locations.

--- init.bat
+++ init.bat
@@ -1,19 +1,19 @@
 @echo OFF
-if EXIST "%VS120COMNTOOLS%" (
-    ECHO FOUND VS12  - CALLING vcvars32.bat
-    call "%VS120COMNTOOLS%\..\..\VC\bin\vcvars32.bat"
+if EXIST "%VS140COMNTOOLS%" (
+    ECHO FOUND VS14  - CALLING vsvars32.bat
+    call "%VS140COMNTOOLS%\vsvars32.bat"
 ) ELSE (
-    ECHO VS NOT FOUND - BUILD WILL FAIL.
+    ECHO VS14 NOT FOUND - BUILD WILL FAIL.
 )
 
-if EXIST "c:\Program Files (x86)\NUnit 2.6.3\bin\nunit-console.exe" (
+if EXIST "c:\Program Files (x86)\NUnit 2.6.4\bin\nunit-console.exe" (
     ECHO FOUND NUnit SKIPPING Installation.
 ) ELSE (
     ECHO Installing NUnit via chocolatey
     choco install NUnit
 )
 
-if EXIST "c:\Chocolatey\bin\nuget.bat" (
+if EXIST "c:\Program Files (x86)\NUGet" (
     ECHO FOUND NUGet SKIPPING Installation.
 ) ELSE (
     ECHO Installing NUGet via chocolatey
