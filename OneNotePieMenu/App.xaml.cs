eb9ce52764e9ad639b2c9b4bca0622ee6221106f
Igor Dvorkin <idvorkin@gmail.com>
2018-04-13 07:02:03 -0700

    Adding OneNotePieMenu template command line parameter which will cause today's template pages to be created if they don't exist.
    * A hack, should be in a seperate application - but fine for now.
    * Useful when launched from a batch launcher since it makes sure template pages are created.

--- OneNotePieMenu/App.xaml.cs
+++ OneNotePieMenu/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OneNoteMenu;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +14,23 @@ namespace OneNotePieMenu
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            // This should be in a seperate application - but overloading for now, since I'd refactor this anyway before long. 
+            var isGenerateDayAndWeekTemplate = e.Args.Contains("template");
+            {
+                GenerateDayAndWeekTemplate();
+            }
+        }
+
+        private static void GenerateDayAndWeekTemplate()
+        {
+            var capabilities = new AllOneNoteCapabilities();
+
+            // Goto X page will create the page if it does not exist.
+            capabilities.DailyPages.GotoThisWeekPage(); // I think this might break on Sunday night
+            capabilities.DailyPages.GotoTodayPage();
+            System.Windows.Application.Current.Shutdown();
+        }
     }
 }
